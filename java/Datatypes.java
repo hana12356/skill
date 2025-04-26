5fc059c71dfb84683b9c9fa793928b3f4d46f49d
hana12356 <156879623+hana12356@users.noreply.github.com>
2025-04-26 12:48:33 +0530

    Add files via upload

---
+++ java/Datatypes.java
@@ -0,0 +1,38 @@
+public class Datatypes {
+    public static void main(String args[]) {
+		int x=100;
+		System.out.println(x);
+		
+		float y=5.25f;
+		System.out.println(y);
+		
+		char hi='k';
+		System.out.println(hi);
+		
+		boolean hello=true;
+		boolean hii=false;
+		System.out.println(hello);
+		System.out.println(hii);
+		
+		short num=10505;
+		System.out.println(num);
+		
+		double d=15.4789236d;
+		System.out.println(d);
+		
+		long l=15000004;
+		System.out.println(l);
+		
+		char val1=68,val2=67,val3=97;
+		System.out.println(val1);
+		System.out.println(val2);
+		System.out.println(val3);
+		
+		int x2 =5;
+		int y2=10;
+		int h=x2+y2;
+		System.out.println(h);
+		
+		
+	}
+}
